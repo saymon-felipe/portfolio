498b8c71fdcdcdf475e329abefd1cf57a5d77e60
Saymon Felipe <linnubr@gmail.com>
2023-12-21 23:06:57 -0300

    mudança da porta da api

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let url_api;
 
 // Troca de ambiente da api.
-const dev_environment = "http://localhost:4800"; //Ambiente de desenvolvimento.
+const dev_environment = "http://localhost:3000"; //Ambiente de desenvolvimento.
 const production_environment = "https://saymon-portfolio-api.herokuapp.com/"; //Ambiente de produção.
 
 let test_or_prod = 1;
