e364f9623ac3c640a3a33d297798a54571f04d7f
Saymon Felipe <linnubr@gmail.com>
2023-12-21 23:03:23 -0300

    troca de ambiente

--- src/configs/api.js
+++ src/configs/api.js
@@ -6,7 +6,7 @@ let url_api;
 const dev_environment = "http://localhost:4800"; //Ambiente de desenvolvimento.
 const production_environment = "https://saymon-portfolio-api.herokuapp.com/"; //Ambiente de produção.
 
-let test_or_prod = 0;
+let test_or_prod = 1;
 
 switch (test_or_prod) {
     case 0:
